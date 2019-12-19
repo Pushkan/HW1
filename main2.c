7b20fd2e82591b6dd7a696c73b35bd26e7f8a7df
Paul <pushkan13@gmail.com>
2019-12-19 11:26:31 +0000

    Заготовка для второй домашки

---
+++ main2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void hw2_1();
+void hw2_2();
+void hw2_2_b();
+void hw2_3();
+
+int main(void) {
+  hw2_1();
+  hw2_2();
+  hw2_2_b();
+  hw2_3();
+  return 0;
+}
+
+void hw2_1(){
+
+}
+
+void hw2_2(){
+
+}
+
+void hw2_2_b(){
+
+}
+
+void hw2_3(){
+
+}
